41c44fe0979c3c85b55ed6f3328bf72af4b0feea
sixsquare <Imonanoko@gmail.com>
2025-09-13 13:42:26 +0000

    python autoql

---
+++ python_autoql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $(basename "$0") <db name> <project root>"
+    exit 1
+fi
+
+codeql_db_dir="/home/sixsquare/codeQL/db"
+output_dir="/home/sixsquare/codeQL/python_query_output/$1"
+ql_dir="/home/sixsquare/codeQL/python-ql"
+echo "建立輸出資料夾: $output_dir"
+mkdir -p "$output_dir"
+ql_list=("CWE-022" "CWE-078" "CWE-079" "CWE-095" "CWE-113" "CWE-117" "CWE-326" "CWE-327" "CWE-329" "CWE-347" "CWE-377" "CWE-502" "CWE-643" "CWE-760" "CWE-918" "CWE-943" "CWE-1333")
+codeql database create "$codeql_db_dir/$1" --language=python --source-root "$2" --threads=0 --overwrite
+for cwe_number in "${ql_list[@]}";do
+    codeql query run "${ql_dir}/${cwe_number}.ql" --database "$codeql_db_dir/$1" --output "${output_dir}/${cwe_number}.bqrs"
+    codeql bqrs decode --format=csv --output "${output_dir}/${cwe_number}.csv" "${output_dir}/${cwe_number}.bqrs"
+done
+rm -f "${output_dir}"/*.bqrs
+exit 0
